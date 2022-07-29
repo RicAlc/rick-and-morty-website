25f00728c19793f420acd6349f33b337ae9c460f
RicAlc <alcala.ricardoc@gmail.com>
2022-07-29 17:39:02 -0500

    6. Rutas de landing y de character

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Character from './routes/Character';
+import Landing from './routes/Landing';
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Route path='/' />
+          <Route path='/' element={<Landing />} />
+          <Route path='/api/characters/:id' element={<Character />} />
         </Routes>
       </BrowserRouter>
     </div>
