238deb39c09a2b3f2ab412892e1c9dd605a81de0
RicAlc <alcala.ricardoc@gmail.com>
2022-07-30 17:49:41 -0500

    8. Removiendo lista de personajes en el landing

--- src/routes/Landing.jsx
+++ src/routes/Landing.jsx
@@ -1,26 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { allCharacters } from '../functions/funciones';
+import React from 'react';
 
 export default function Landing() {
-  const [character, setCharacter] = useState(null);
-  useEffect(() => {
-    allCharacters(setCharacter);
-  }, []);
-
   return (
     <div>
       <h1 className='title'>Página de inicio</h1>
-      {character !== null
-        ? character.map((item) => {
-            return (
-              <li>
-                <a href={`/api/characters/${item.id}`} key={item.id}>
-                  {item.name}
-                </a>
-              </li>
-            );
-          })
-        : 'no hay personajes'}
+      <a href='/api/characters/page/1'>Ver lista de personajes</a>
     </div>
   );
 }
