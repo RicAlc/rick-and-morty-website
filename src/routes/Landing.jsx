c270467cc561b1685a514611ca461bbaf415a481
RicAlc <alcala.ricardoc@gmail.com>
2022-07-30 23:09:56 -0500

    18. agregando boton de personaje aleatorio

--- src/routes/Landing.jsx
+++ src/routes/Landing.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
 export default function Landing() {
+  const randomId = () => {
+    let randomId = Math.floor(Math.random() * 825 + 1);
+    return randomId;
+  };
+  const randomCharacter = () => {
+    window.open(`/api/characters/${randomId()}`, '_self');
+  };
+
   return (
     <div>
       <h1 className='title'>Página de inicio</h1>
       <a href='/api/characters/page/1'>Ver lista de personajes</a>
+      <button className='random-char' onClick={randomCharacter}>
+        Personaje aleatorio
+      </button>
     </div>
   );
 }
