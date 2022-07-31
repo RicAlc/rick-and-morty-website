9cb262d0a0e69cd8d5a55717f67b824608e9ed19
RicAlc <alcala.ricardoc@gmail.com>
2022-07-30 20:00:50 -0500

    12. Quitando mensajes en consola

--- src/functions/funciones.js
+++ src/functions/funciones.js
@@ -9,14 +9,12 @@ const getCharacter = async (id, state) => {
   const request = await axios.get(
     `https://rickandmortyapi.com/api/character/${id}`
   );
-  console.log(request.data);
   state(request.data);
 };
 const getCharacterList = async (page, state) => {
   const request = await axios.get(
     `https://rickandmortyapi.com/api/character/?page=${page}`
   );
-  console.log(request.data);
   state(request.data);
 };
 
